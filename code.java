8e569bf3be7cb67465b61c1c09d8c17ef646dbd3
AbhishekKmrSingh <91712759+AbhishekKmrSingh@users.noreply.github.com>
2022-10-13 16:18:30 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+class Main {
+    public static void main(String[] args) {
+        LinkedList<String> animals= new LinkedList<>();
+
+        // Add elements in LinkedList
+        animals.add("Dog");
+        animals.add("Horse");
+        animals.add("Cat");
+
+        // Create an object of ListIterator
+        ListIterator<String> listIterate = animals.listIterator();
+        System.out.print("LinkedList: ");
+
+        while(listIterate.hasNext()) {
+            System.out.print(listIterate.next());
+            System.out.print(", ");
+        }
+
+  //help creating code
+        }
+    }
+}
